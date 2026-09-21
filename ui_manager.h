f6e8e9400929593064d07793b91b31e64026ba8c
fredrikrudin <49065026+fredrikrudin@users.noreply.github.com>
2026-09-21 08:16:57 +0200

    Create ui_manager.h

---
+++ ui_manager.h
@@ -0,0 +1,13 @@
+#ifndef UI_MANAGER_H
+#define UI_MANAGER_H
+
+#include "lvgl.h"
+
+namespace UIManager {
+    void init();
+    void update();
+    void showMainScreen();
+    void showSettingsScreen();
+}
+
+#endif
